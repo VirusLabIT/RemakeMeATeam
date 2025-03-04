9afef54afe831b5fb5bc18e4b1dbb26fbf20c79a
JakubZielinskii <kubaleczek1@gmail.com>
2025-03-04 22:13:37 +0100

    Sorted files, Changed few lines of code
    Sorted files into some folders, that should be easy to understand for a normal human. Changed few lines of code. Removed useless line and changed it with slightly simpler alternative. Changed the name of one variable to also something more readable and reasonable. Added the comment (mostly for myself) regarding the name of this script. Please use "...Manager" next time...

---
+++ Assets/Scripts/ScenesMGR.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesMGR : MonoBehaviour
+{
+    // MGR STANDS FOR MANAGER !!!!!
+
+    private static ScenesMGR instance;
+
+    public static ScenesMGR Instance
+    {
+        get
+        {
+            if (instance.TryGetComponent<ScenesMGR>(out ScenesMGR manager))
+            {
+                GameObject sceneManagerObject = new GameObject();
+                instance = sceneManagerObject.AddComponent<ScenesMGR>();
+                sceneManagerObject.name = typeof(Player).ToString() + " (Singleton)";
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void LoadScene(string sceneName, string sceneName2)
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        foreach (var item in SceneManager.GetSceneByName(sceneName2).GetRootGameObjects())
+        {
+            item.SetActive(false);
+        }
+    }
+
+    public void UnloadScene(string sceneName1, string sceneName2)
+    {
+        SceneManager.UnloadSceneAsync(sceneName1);
+        foreach (var item in SceneManager.GetSceneByName(sceneName2).GetRootGameObjects())
+        {
+            item.SetActive(true);
+        }
+    }
+
+}
